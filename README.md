70a1b65ca1aef1f604105045c1154364de13c957
Al_Capuchino <claimix111@gmail.com>
2023-04-10 22:35:53 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,46 @@
+<h2 align="center">Kanban Board</h2>
+
+### Описание проекта
+auth, DRF
+
+
+**Стек**
+- Python >= 3.10
+- Django >= 4.1.7
+- sqlite3
+
+Установка
+
+pip install req.txt
+
+Старт
+
+python manage.py runserver
+
+## Разработка
+#### 1) Поставить звездочку)
+#### 2) Клонировать репозиторий
+git clone https://github.com/True-Ha/Kanban_board.git
+
+#### 3) Создать виртуальное окружение
+cd kanban
+
+python -m venv venv
+#### 4) Активировать виртуальное окружение
+Linux:
+source venv/bin/activate
+Windows:
+./venv/Scripts/activate
+#### 5) Устанавливить зависимости:
+pip install -r req.txt
+#### 6) Выполнить команду для выполнения миграций
+python manage.py migrate
+#### 7) Создать суперпользователя
+python manage.py createsuperuser
+#### 8) Запустить сервер
+python manage.py runserver
+#### 9) Ссылки
+Сайт http://127.0.0.1:8000/
+
+Админ панель http://127.0.0.1:8000/admin
+
---
+++ api/views.py
@@ -0,0 +1,29 @@
+from api.serializers import TaskSerializer
+
+from board.models import Task
+from rest_framework import generics
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+class ListTask(generics.ListAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Task.objects.filter(owner=user)
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class DetailTask(generics.RetrieveUpdateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Task.objects.filter(owner=user)
